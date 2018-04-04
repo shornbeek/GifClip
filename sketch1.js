56f340b575ddee2770a80c7d264edfea69dd0e7d
Stephen H <hbeeken@hotmail.com>
2018-04-03 17:21:05 -0700

    algo changes on sketch1 and sketch
    added in fail page. 
    added in safe for hot page

--- sketch1.js
+++ sketch1.js
@@ -1,4 +1,35 @@
 
+var api = "https://api.giphy.com/v1/gifs/search?";
+var apiKey = "&api_key=yDCCeSvcpPbQ6VyCgzSv1zTTlb8gLQIx";
+var query = "&q=fail+funny&limit=122&offset=0&lang=en&rating=pg-13";
+
+
+
+function setup() {
+	noCanvas();
+    var url = api + apiKey + query;
+    loadJSON(url, gotData);
+}
+
+
+
+
+function setup1() { 
+	noCanvas();
+    var url = api + apiKey + query;
+    loadJSON(url, gotData);
+}
+
+function gotData(gif){
+    for (var i = 0; i < gif.data.length; i++){
+        
+    createImg(gif.data[i].images.fixed_height.url);
+    
+    }
+};
+
+
+
 
 
 
