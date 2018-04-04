c7afecab6f465452086aec4e50e755d58728243e
Stephen H <hbeeken@hotmail.com>
2018-04-03 19:41:31 -0700

    aligned css styles for all pages
    css in news and index pages

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
 var api = "https://api.giphy.com/v1/gifs/search?";
 var apiKey = "&api_key=yDCCeSvcpPbQ6VyCgzSv1zTTlb8gLQIx";
-var query = "&q=sexy+boobs+girl&limit=62&offset=0&lang=en&rating=pg-13";
+var query = "&q=sexy+boobs+woman&limit=62&offset=0&lang=en&rating=pg-13";
 
 
 
